974dddcc4f74adb807790229167839291978c7ee
tkim999 <theodorekim@callutheran.edu>
2021-12-09 03:03:38 -0800

    updated

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -14,7 +14,7 @@ public class Database
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jeffery","root","passwordTeam6");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select email from usercredentials where username=\"pog\"");
+            ResultSet rs = stmt.executeQuery("select password from usercredentials where username<=\"tom\"");//"insert into usercredentials (db_username,db_password,db_email,db_lockoutCount) values (fred,wow4,fred@callutheran.edu,0)"
             while(rs.next()) {
                 System.out.println(rs.getString(1));
             }
